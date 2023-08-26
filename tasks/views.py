bae71e5e89163f8a35c9e788e2a6273a4ca1a4d1
Diego Valencia <ve18007@ues.edu.sv>
2023-08-25 21:25:59 -0600

    test

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+tasks_list = []  # Lista para almacenar las tareas
+
+
+def task_list(request):
+    return render(request, 'tasks/task_list.html', {'tasks': tasks_list})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        tasks_list.append(title)
+        return redirect('task_list')
+    return render(request, 'tasks/add_task.html')
